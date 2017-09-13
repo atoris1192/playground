32546062a1f88c493175d281a72e7fe3546d2e0a
k.niuishi <atoris1192@gmail.com>
2017-09-13 11:53:06 +0900

    size M L 追加

--- pug/gulp-pugで開発環境を作る/src/assets/js/common.js
+++ pug/gulp-pugで開発環境を作る/src/assets/js/common.js
@@ -1,5 +1,7 @@
-new Masonry('body', {
-  itemSelector: '.item',
-  columnWidth: 180,
-  gutter: 4
-});
+window.onload = function() { // 画像読み込み終了後、実行 重い画像があると表示が遅れるので、画像の高さ指定での対応を推奨
+  new Masonry('body', {
+    itemSelector: '.item',
+    columnWidth: 180,
+    gutter: 4
+  });
+}
